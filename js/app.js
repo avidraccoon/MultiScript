488bc81e41d71903bee8b2696c05f1f6809d69c1
nathansmith <gonathansmith@gmail.com>
2024-12-25 12:50:48 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,43 @@
+const context = new CodeContext();
+const output = new ElementOutput("output");
+context.setOutput(output);
+// let test;
+// test = 3 * 4;
+// while (test){
+//   console.log(test);
+//   test -= 1;
+// }
+// console.log(test);
+
+const value1 = new ValueStatement(3);
+const value2 = new ValueStatement(4);
+const value3 = new ValueStatement(1);
+
+const createVariable = new VariableCreation("test");
+
+const multiplication = new MultiplicationStatement(value1, value2);
+const variable = new VariableAssignment("test", multiplication);
+
+const whileLoopCodeBlock = new CodeBlock();
+const whileVariableValue = new VariableStatement("test");
+const whileLoopPrint = new PrintLine(whileVariableValue);
+const subtractVariable = new SubtractionStatement(whileVariableValue, value3);
+const whileLoopVariable = new VariableAssignment("test", subtractVariable);
+whileLoopCodeBlock.addLine(whileLoopPrint);
+whileLoopCodeBlock.addLine(whileLoopVariable);
+
+const variableValue = new VariableStatement("test");
+const whileLoop = new WhileStatement(variableValue, whileLoopCodeBlock);
+
+const endVariableValue = new VariableStatement("test");
+const endValuePrint = new PrintLine(endVariableValue);
+
+const codeBlock = new CodeBlock();
+codeBlock.addLine(createVariable);
+codeBlock.addLine(variable);
+codeBlock.addLine(whileLoop)
+codeBlock.addLine(endValuePrint);
+
+codeBlock.execute(context);
+//console.log(context.getVariable("test"));
+
---
+++ js/compiler.js
@@ -0,0 +1,755 @@
+
+class Output{
+
+  clear(){
+    console.clear()
+  }
+
+  print(text){
+    console.log(text);
+  }
+
+  println(text) {
+    console.log(text);
+  }
+}
+
+class ElementOutput extends Output{
+  constructor(id) {
+    super();
+    this.elementId = id;
+    this.element = document.getElementById(id);
+    this.text = '';
+  }
+
+  clear(){
+    this.element.innerText = '';
+    this.text = '';
+  }
+
+  print(text){
+    this.text += text;
+    this.element.innerHTML = this.text;
+  }
+
+  println(text){
+    this.print(text+'\n')
+  }
+
+}
+
+class Scope{
+
+  constructor() {
+    this.variables = {};
+    this.methods = {};
+  }
+
+  setMethod(methodName, method) {
+    this.methods[methodName] = method;
+  }
+
+  getMethod(methodName) {
+    return this.methods[methodName];
+  }
+
+  setVariable(variableName, variableValue) {
+    this.variables[variableName] = variableValue;
+  }
+
+  getVariable(variableName) {
+    return this.variables[variableName];
+  }
+
+  hasVariable(variableName) {
+    return !!(this.variables[variableName] || this.variables[variableName] == null);
+  }
+
+  createVariable(name) {
+    this.variables[name] = null;
+  }
+
+}
+
+class ScopeHandler{
+
+  constructor(){
+    this.scopes = [new Scope()]
+  }
+
+  getMethod(methodName) {
+    let method = undefined;
+    for (let i=0; i<this.scopes.length; i++) {
+      method = this.scopes[i].getMethod(methodName);
+      if (method) return method;
+    }
+    //TODO make error on not found
+    return method;
+  }
+
+  getVariable(methodName) {
+    let variable = undefined;
+    for (let i=0; i<this.scopes.length; i++) {
+      variable = this.scopes[i].getVariable(methodName);
+      if (variable) return variable;
+    }
+    //TODO make error on not found
+    return variable;
+  }
+
+  setVariable(variableName, variableValue) {
+    for (let i=0; i<this.scopes.length; i++) {
+      if (this.scopes[i].hasVariable(variableName)) {
+        this.scopes[i].setVariable(variableName, variableValue);
+        return;
+      }
+    }
+    //TODO make error on not found
+  }
+
+  createVariable(variableName){
+    this.scopes[this.scopes.length-1].createVariable(variableName);
+  }
+
+  increaseScope(){
+    this.scopes.push(new Scope());
+  }
+
+  decreaseScope(){
+    this.scopes.pop();
+  }
+
+}
+
+//TODO add more context methods
+class CodeContext{
+  constructor() {
+    this.scopeHandler = new ScopeHandler();
+    this.output = new Output();
+  }
+
+  setOutput(output){
+    this.output = output;
+  }
+
+  getMethod(methodName) {
+    return this.scopeHandler.getMethod(methodName);
+  }
+
+  getVariable(methodName) {
+    return this.scopeHandler.getVariable(methodName);
+  }
+
+  setVariable(variableName, variableValue) {
+    this.scopeHandler.setVariable(variableName, variableValue);
+  }
+
+  createVariable(variableName) {
+    this.scopeHandler.createVariable(variableName);
+  }
+
+  print(text){
+    this.output.print(text);
+  }
+
+  println(text){
+    this.output.println(text);
+  }
+
+  clear(){
+    this.output.clear();
+  }
+}
+
+class ReturnToken{
+  static Normal = 0;
+  static Return = 1;
+  static Error = 2;
+  static None = 3;
+  constructor(type, ret, data){
+    this.type = type;
+    this.ret = ret;
+    this.data = data;
+  }
+}
+
+class Code{
+  execute(context){
+
+  }
+}
+
+class Statement extends Code{
+  evaluate(context){
+    const returnToken = this.execute(context);
+    return returnToken.ret;
+  }
+
+  execute(context){
+
+  }
+}
+
+class ValueStatement extends Statement{
+
+  constructor(value) {
+    super();
+    this.value = value;
+  }
+
+  execute(context) {
+    return new ReturnToken(ReturnToken.Normal, this.value);
+  }
+}
+
+class VariableStatement extends Statement{
+
+  constructor(variableName) {
+    super();
+    this.variableName = variableName;
+  }
+
+  execute(context) {
+    return new ReturnToken(ReturnToken.Normal, context.getVariable(this.variableName));
+  }
+
+}
+
+//TODO add custom implementation of Addition
+class AdditionStatement extends Statement{
+
+  constructor(statement1, statement2) {
+    super();
+    this.statement1 = statement1;
+    this.statement2 = statement2;
+  }
+
+  execute(context) {
+    const result = this.statement1.evaluate(context) + this.statement2.evaluate(context);
+    return new ReturnToken(ReturnToken.Normal, result);
+  }
+
+}
+
+//TODO add custom implementation of Subtraction
+class SubtractionStatement extends Statement{
+
+  constructor(statement1, statement2) {
+    super();
+    this.statement1 = statement1;
+    this.statement2 = statement2;
+  }
+
+  execute(context) {
+    const result = this.statement1.evaluate(context) - this.statement2.evaluate(context);
+    return new ReturnToken(ReturnToken.Normal, result);
+  }
+
+}
+
+//TODO add custom implementation of Multiplication
+class MultiplicationStatement extends Statement{
+
+  constructor(statement1, statement2) {
+    super();
+    this.statement1 = statement1;
+    this.statement2 = statement2;
+  }
+
+  execute(context) {
+    const result = this.statement1.evaluate(context) * this.statement2.evaluate(context);
+    return new ReturnToken(ReturnToken.Normal, result);
+  }
+
+}
+
+//TODO add custom implementation of Division
+class DivisionStatement extends Statement{
+
+  constructor(statement1, statement2) {
+    super();
+    this.statement1 = statement1;
+    this.statement2 = statement2;
+  }
+
+  execute(context) {
+    const result = this.statement1.evaluate(context) / this.statement2.evaluate(context);
+    return new ReturnToken(ReturnToken.Normal, result);
+  }
+
+}
+
+//TODO add custom implementation of Equals
+class EqualsStatement extends Statement{
+
+  constructor(statement1, statement2) {
+    super();
+    this.statement1 = statement1;
+    this.statement2 = statement2;
+  }
+
+  execute(context) {
+    const result = this.statement1.evaluate(context) == this.statement2.evaluate(context);
+    return new ReturnToken(ReturnToken.Normal, result);
+  }
+
+}
+
+//TODO add custom implementation of Less Than
+class LessThanStatement extends Statement{
+
+  constructor(statement1, statement2) {
+    super();
+    this.statement1 = statement1;
+    this.statement2 = statement2;
+  }
+
+  execute(context) {
+    const result = this.statement1.evaluate(context) < this.statement2.evaluate(context);
+    return new ReturnToken(ReturnToken.Normal, result);
+  }
+
+}
+
+//TODO add custom implementation of Greater Than
+class GreaterThanStatement extends Statement{
+
+  constructor(statement1, statement2) {
+    super();
+    this.statement1 = statement1;
+    this.statement2 = statement2;
+  }
+
+  execute(context) {
+    const result = this.statement1.evaluate(context) < this.statement2.evaluate(context);
+    return new ReturnToken(ReturnToken.Normal, result);
+  }
+
+}
+
+class AndStatement extends Statement{
+
+  constructor(statement1, statement2) {
+    super();
+    this.statement1 = statement1;
+    this.statement2 = statement2;
+  }
+
+  execute(context) {
+    const result = this.statement1.evaluate(context) && this.statement2.evaluate(context);
+    return new ReturnToken(ReturnToken.Normal, result);
+  }
+
+}
+
+class OrStatement extends Statement{
+
+  constructor(statement1, statement2) {
+    super();
+    this.statement1 = statement1;
+    this.statement2 = statement2;
+  }
+
+  execute(context) {
+    const result = this.statement1.evaluate(context) || this.statement2.evaluate(context);
+    return new ReturnToken(ReturnToken.Normal, result);
+  }
+
+}
+
+class ConditionalStatement extends Statement{
+  constructor(statement) {
+    super();
+    this.statement = statement;
+  }
+
+  execute(context){
+    return this.statement.execute(context);
+  }
+
+  evaluate(context){
+    return Boolean(super.evaluate(context));
+  }
+}
+
+class CodeBlock extends Code {
+  constructor() {
+    super();
+    this.lines = [];
+  }
+
+  addLine(line){
+    this.lines.push(line);
+  }
+
+  execute(context) {
+    for (let line of this.lines) {
+      if (line instanceof ReturnStatement ) {
+        return new ReturnToken(ReturnToken.Return, line.execute(context));
+      }
+      //console.log(line)
+      line.execute(context);
+    }
+    return new ReturnToken(ReturnToken.Normal);
+  }
+}
+
+class ReturnStatement extends Statement{
+  constructor(statement) {
+    super();
+    this.statement = statement;
+  }
+
+  execute(context) {
+    return new ReturnToken(ReturnToken.Return, this.statement.execute(context).ret);
+  }
+}
+
+class Method {
+
+  constructor(code_block) {
+    this.code_block = code_block;
+  }
+
+  execute(context){
+    return new ReturnToken(ReturnToken.Normal, this.code_block.execute(context));
+  }
+}
+
+class MethodCall extends Statement{
+
+  constructor(methodName) {
+    super();
+    this.methodName = methodName;
+  }
+
+  execute(context){
+    return context.getMethod(this.methodName).execute(context);
+  }
+}
+
+class VariableAssignment extends Code{
+  constructor(variableName, statement) {
+    super();
+    this.variableName = variableName;
+    this.statement = statement;
+  }
+
+  execute(context) {
+    context.setVariable(this.variableName, this.statement.evaluate(context))
+    return new ReturnToken(ReturnToken.None);
+  }
+}
+
+class VariableCreation extends Code{
+  constructor(variableName) {
+    super();
+    this.variableName = variableName;
+  }
+
+  execute(context) {
+    context.createVariable(this.variableName);
+    return new ReturnToken(ReturnToken.None);
+  }
+}
+
+class IfStatement extends Code{
+
+  constructor(statement, code_block, else_statement) {
+    super();
+    this.conditional_statement = new ConditionalStatement(statement);
+    this.code_block = code_block;
+    if (else_statement){
+      this.else_statement = else_statement;
+    }else{
+      this.else_statement = undefined;
+    }
+  }
+
+  execute(context){
+    if (this.conditional_statement.evaluate(context)){
+      this.code_block.execute(context);
+    }else if (this.else_statement){
+      this.else_statement.execute(context);
+    }
+  }
+
+}
+
+class ElseStatement extends Code {
+
+  constructor(code_block) {
+    super();
+    this.code_block = code_block;
+  }
+
+  execute(context) {
+    this.code_block.execute(context);
+  }
+
+}
+
+class ElseIfStatement extends IfStatement {
+  constructor(statement, code_block, else_statement) {
+    super(statement, code_block, else_statement);
+  }
+}
+
+class WhileStatement extends Code{
+  constructor(statement, code_block) {
+    super();
+    this.conditional_statement = new ConditionalStatement(statement);
+    this.code_block = code_block;
+  }
+
+  execute(context) {
+    while (this.conditional_statement.evaluate(context)){
+      this.code_block.execute(context);
+    }
+  }
+}
+
+class ForLoop extends Code{
+  constructor(init_statement, condition, loop_end, loop_code) {
+    super();
+    this.init_statement = init_statement;
+    this.condition = new ConditionalStatement(condition);
+    this.loop_end = loop_end;
+    this.loop_code = loop_code;
+  }
+
+  execute(context) {
+    this.init_statement.execute(context);
+    while (this.conditional_statement.evaluate(context)){
+      this.loop_code.execute(context);
+      this.loop_end.execute(context);
+    }
+  }
+}
+
+class Print extends Code {
+  constructor(statement) {
+    super();
+    this.statement = statement;
+  }
+
+  execute(context) {
+    context.print(this.statement.evaluate(context));
+    return new ReturnToken(ReturnToken.None);
+  }
+}
+
+class PrintLine extends Code {
+  constructor(statement) {
+    super();
+    this.statement = statement;
+  }
+
+  execute(context) {
+    context.println(this.statement.evaluate(context));
+    return new ReturnToken(ReturnToken.None);
+  }
+}
+
+class Token {
+  constructor(type, line_number=0, character_number=0) {
+    this.type = type;
+    this.line_number = line_number;
+    this.character_number = character_number;
+    this.content = "";
+  }
+}
+
+class Tokens {
+  static IDENTITY_TOKEN = 0;
+  static METHOD_TOKEN = 1;
+  static METHOD_CALL_TOKEN = 2;
+  static RETURN_TOKEN = 3;
+  static IF_TOKEN = 4;
+  static ELSE_IF_TOKEN = 5;
+  static ELSE_TOKEN = 6;
+  static WHILE_TOKEN = 7;
+  static FOR_TOKEN = 8;
+  static SPACE_TOKEN = 9;
+}
+
+class TokenResponse {
+  constructor(hasToken=false, char_count=0, token, content) {
+    this.hasToken = hasToken;
+    this.char_count = char_count;
+    this.token = token;
+    this.content = content;
+  }
+}
+
+class TokenMatch {
+  constructor(level, pattern, match, offset){
+    this.level = level;
+    this.pattern = pattern;
+    this.match = match;
+    this.position = match.index+offset;
+  }
+}
+
+class TokenPattern {
+  static Patterns = new Map();
+
+  static setPattern(ID, pattern){
+    this.Patterns.set(ID, pattern);
+  }
+
+  static getPattern(ID){
+    return this.Patterns.get(ID);
+  }
+
+  static getMatchingPattern(text, end_of_line){
+    const keys = Array.from(this.Patterns.keys());
+    const patterns = keys.map((key) => this.Patterns.get(key));
+    patterns.sort((a, b) => a.level-b.level);
+    for (let i = 0; i < patterns.length; i++) {
+      const pattern = patterns[i];
+      const match = pattern.pattern.exec(text);
+      if (match){
+        if (end_of_line || match[0].length < text.length || pattern.terminating){
+          return new TokenResponse(true, match[0].length, new Token(pattern.type), match[0]);
+        }
+      }
+    }
+    return new TokenResponse();
+  }
+
+  constructor(type, pattern = new RegExp(""), level = 999, terminating = true) {
+    this.pattern = pattern;
+    this.level = level;
+    this.terminating = terminating;
+    this.type = type;
+  }
+}
+TokenPattern.setPattern(Tokens.IDENTITY_TOKEN,
+  new TokenPattern(Tokens.IDENTITY_TOKEN, /\w+/, 9, false));
+TokenPattern.setPattern(Tokens.SPACE_TOKEN,
+  new TokenPattern(Tokens.SPACE_TOKEN, / /));
+TokenPattern.setPattern(Tokens.RETURN_TOKEN,
+  new TokenPattern(Tokens.RETURN_TOKEN, /return/));
+console.log(TokenPattern.getPattern(Tokens.IDENTITY_TOKEN));
+
+class TokenList {
+  constructor(tokens) {
+    this.tokens = []
+    if (tokens){
+      this.tokens = tokens;
+    }
+  }
+
+  addToken(token){
+    this.tokens.push(token);
+  }
+
+  startParsing(){
+    this.tokens.reverse();
+  }
+
+  consumeTokens(count){
+    for (let i = 0; i < count; i++) {
+      this.tokens.pop();
+    }
+  }
+
+  consumeToken(){
+    this.consumeTokens(1);
+  }
+
+  peekAhead(distance){
+    return this.tokens[this.tokens.length - 1 - distance];
+  }
+
+  peek(){
+    return this.peekAhead(1);
+  }
+
+  getToken(){
+    return this.tokens.pop();
+  }
+
+  getTokens(count){
+    const retTokens = [];
+    for (let i = 0; i < count; i++) {
+      retTokens.push(this.getToken());
+    }
+    return new TokenList(retTokens);
+  }
+
+  distanceToNext(tokenType){
+    for (let i = 0; i < this.tokens.length; i++) {
+      if (this.tokens[this.tokens.length-1-i].type === tokenType){
+        return i;
+      }
+    }
+    return -1;
+  }
+}
+
+class Lexer{
+
+  constructor() {
+
+  }
+
+  handleTokenGeneration(tokens, text, line_num, character_num, end_of_line){
+    let remaining_text = text;
+    let response;
+    do {
+      response = TokenPattern.getMatchingPattern(remaining_text, end_of_line);
+      //console.log(response)
+      if (response.hasToken){
+        const token = response.token;
+        token.line_number = line_num;
+        token.character_number = character_num-response.char_count+1;
+        token.content = response.content;
+        tokens.addToken(token);
+      }
+      remaining_text = remaining_text.slice(response.char_count);
+
+    }while (response.hasToken);
+    return remaining_text;
+  }
+
+  tokenize(text) {
+    const tokens = new TokenList();
+    let lines = text.split("\n");
+    let line_number = 1;
+    for (let line of lines) {
+      let character_number = 1;
+      let current_text = "";
+      const line_length = line.length;
+      for (let char of line) {
+        current_text+=char;
+        current_text = this.handleTokenGeneration(tokens, current_text, line_number, character_number, character_number===line_length);
+        character_number++;
+      }
+      line_number++;
+    }
+    for (let token of tokens.tokens){
+      console.log(token)
+    }
+    return tokens;
+  }
+}
+
+class Parser {
+}
+
+class Compiler {
+  constructor(){}
+
+  compile(code){}
+
+}
+
+const lexer = new Lexer();
+function run(){
+  const code = document.getElementById("code");
+  lexer.tokenize(code.value);
+}
+
+document.getElementById("run").onclick = run;
